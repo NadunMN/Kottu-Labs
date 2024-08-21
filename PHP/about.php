097be43349f24b58d8e3ed21dbe98bb5091fbd9b
Thirani Imanya <thiraniwork@gmail.com>
2024-08-21 15:28:23 +0530

    Update About

---
+++ PHP/about.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About us</title>
+    <link rel="stylesheet" href="../CSS/navbar.css">
+    <link rel="stylesheet" href="../CSS/about.css">
+    <link rel="stylesheet" href="../CSS/footer.css">
+</head>
+
+<body>
+    <?php //include 'navbar.php'; ?>
+
+        <section id="aboutHeader">
+            <div class="container">
+                <div class="title">
+                    <h2>About Us</h2>
+                </div>
+            </div>
+        </section>
+
+        <section id="aboutPageSec">
+            <div class="container">
+                <h2>Welcome! <span class="pClr">KOTTU LABS</span> <br> a cozy restaurant offering a large variety of kottu dishes, served in an inviting atmosphere with attentive service.</h2>
+            </div>
+        </section>
+        
+        <section id="MidSec">
+            <div class="container1">
+                
+                <div class="card">
+                    <img src="../Photo/Thirani_pics/Wattala.png" alt="Wattala">
+                    <div class="intro">
+                        <h1>Wattala Branch</h1>
+                        <p>Kottu Labs' <span>Wattala branch</span> offers a vibrant dining experience, specializing in a wide variety of freshly made kottu dishes. With its modern ambiance and friendly staff, 
+                            it's the perfect spot to enjoy Sri Lanka's beloved street food with a creative twist.</p>
+                    </div>
+
+                    <div class="card">
+                    <img src="../Photo/Thirani_pics/Kelaniya.jpg" alt="Kelaniya">
+                    <div class="intro">
+                        <h1>Kelaniya Branch</h1>
+                        <p>Kottu Labs' <span>Kelaniya branch</span> offers a vibrant dining experience, specializing in a wide variety of freshly made kottu dishes. With its modern ambiance and friendly staff, 
+                            it's the perfect spot to enjoy Sri Lanka's beloved street food with a creative twist.</p>
+                    </div>
+
+                    <div class="card">
+                    <img src="../Photo/Thirani_pics/Kotahena.jpg" alt="Kotahena">
+                    <div class="intro">
+                        <h1>Kotahena Branch</h1>
+                        <p>Kottu Labs' <span>Kotahena branch</span> offers a vibrant dining experience, specializing in a wide variety of freshly made kottu dishes. With its modern ambiance and friendly staff, 
+                            it's the perfect spot to enjoy Sri Lanka's beloved street food with a creative twist.</p>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+
+    <?php //include 'footer.php'; ?>
+</body>
+</html>
