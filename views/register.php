b47803a9536a1c8fb21a5ca9ccba39d05187f1a5
Nadun Madusanka <140165855+NadunMN@users.noreply.github.com>
2024-11-01 22:55:10 +0530

    update User login

--- views/register.php
+++ views/register.php
@@ -3,6 +3,8 @@
 
 <?php $form = \app\core\form\Form::begin('', 'post')?>
 
+<?php echo $form->field($model, 'firstname') ?>
+<?php echo $form->field($model, 'lastname') ?>
 <?php echo $form->field($model, 'email') ?>
 <?php echo $form->field($model, 'password')->passwordField() ?>
 <?php echo $form->field($model, 'confirmPassword')->passwordField() ?>
