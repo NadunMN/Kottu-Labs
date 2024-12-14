2ec0124a9560d2b493f31ccf1467e40c46237ca0
Nadun Madusanka <140165855+NadunMN@users.noreply.github.com>
2024-12-14 23:42:10 +0530

    New migrations

---
+++ migrations/m0002_addBranch.php
@@ -0,0 +1,32 @@
+<?php
+
+use app\core\Application;
+
+class m0002_addBranch
+{
+    public function up()
+    {
+        $db = Application::$app->db;
+
+        // Add branches data
+        $checkColumnSQL = "SELECT * FROM `branches`;";
+        $result = $db->pdo->query($checkColumnSQL)->fetch();
+        if (!$result) {
+            $SQL = "INSERT INTO branches (branch_id, branch_name)
+                    VALUES
+                    (1, 'Wattala'),
+                    (2, 'Kelaniya'),
+                    (3, 'Kotahena');";
+            $db->pdo->exec($SQL);
+        }
+    }
+
+    public function down()
+    {
+        $db = Application::$app->db;
+
+        // Drop branches table
+        $SQL = "DROP TABLE IF EXISTS branches;";
+        $db->pdo->exec($SQL);
+    }
+}
