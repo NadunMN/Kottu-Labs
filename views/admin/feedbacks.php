8fcc3432cd86ae4780f8b9355b05c1cf3833892a
Nadun Madusanka <140165855+NadunMN@users.noreply.github.com>
2024-12-15 15:52:59 +0530

    Full admin dashboard

--- views/admin/feedbacks.php
+++ views/admin/feedbacks.php
@@ -1,1 +1,19 @@
-<h1>feedbacks</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/CSS/managerDashboard.css">
+</head>
+
+<body>
+
+    <div class="view-branch-menu-section" id="main-content">   
+    </div>
+
+    <script src="/JavaScript/admin/feedbacks.js"></script>
+</body>
+
+</html>
