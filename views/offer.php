67686de178dacd44d8bc8e69e9932d5b92afbbd5
Nadun Madusanka <140165855+NadunMN@users.noreply.github.com>
2025-01-08 01:11:49 +0530

    Modify the offer page

--- views/offer.php
+++ views/offer.php
@@ -1,109 +1,126 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>cart</title>
-    <link rel="stylesheet" href="/CSS/offer.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Menu - Kottu Labs</title>
+  <link rel="stylesheet" href="/CSS/offer.css">
 </head>
+
 <body>
 
-<div class="main-wrapper">
-
-
-
-<div class="cart-wrapper">
-    <div class="card-topic-container">
-        <h1>Offer Section</h1>
-        <h5>20 offers available</h5> 
-    </div>
-
-    <div class="card-section">
-
-  <div class="card">
-    <img src="/Photo/offers/AZEEZ RESTAURANT.jpg" alt="Product Image" class="card-image">
-    <div class="card-content">
-      <h2 class="card-title">Product Name</h2>
-      <div class="card-price">Rs. 1000/=</div>
-      <p class="card-description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
-      </p>
-      <button class="view-button">View Details</button>
-    </div>
+  <div class="menu-topic">
+    <h1>Kottu Labs Offers</h1>
+    <p>At Kottu Labs, Your dining experience with our exclusive special deals!</p>
   </div>
 
-  <div class="card">
-    <img src="/Photo/offers/_Start your forever with a delightful symphony of flavors at our wedding breakfast!.jpg" alt="Product Image" class="card-image">
-    <div class="card-content">
-      <h2 class="card-title">Product Name</h2>
-      <div class="card-price">Rs. 2500/=</div>
-      <p class="card-description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
-      </p>
-      <button class="view-button">View Details</button>
+  <div class="search-container-wrapper">
+
+    <div class="search-container">
+
+
+
+      <select class="search-select">
+        <option value="All">All</option>
+        <option value="All">Wattala</option>
+        <option value="All">Kelaniya</option>
+        <option value="All">Kotahena</option>
+      </select>
+
+      <div class="search-bar">
+        <input type="text" id="search" placeholder="Search menu items...">
+        <div class="search-icon">
+          <img src="/Photo/icon/search.png" alt="">
+          <button>Search</button>
+        </div>
+      </div>
+
+
+
+
     </div>
-  </div>
-
-
-  <div class="card">
-    <img src="/Photo/offers/Premium PSD _ Social media food promotion post and instagram or facebook post banner flyer design template.jpg" alt="Product Image" class="card-image">
-    <div class="card-content">
-      <h2 class="card-title">Product Name</h2>
-      <div class="card-price">Rs. 760/=</div>
-      <p class="card-description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
-      </p>
-      <button class="view-button">View Details</button>
-    </div>
-  </div>
-
-
-  <div class="card">
-    <img src="/Photo/offers/AZEEZ RESTAURANT.jpg" alt="Product Image" class="card-image">
-    <div class="card-content">
-      <h2 class="card-title">Product Name</h2>
-      <div class="card-price">Rs. 1500/=</div>
-      <p class="card-description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
-      </p>
-      <button class="view-button">View Details</button>
-    </div>
-  </div>
-
-  <div class="card">
-    <img src="/Photo/offers/AZEEZ RESTAURANT.jpg" alt="Product Image" class="card-image">
-    <div class="card-content">
-      <h2 class="card-title">Product Name</h2>
-      <div class="card-price">Rs. 500/=</div>
-      <p class="card-description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
-      </p>
-      <button class="view-button">View Details</button>
-    </div>
-  </div>
-
-  
-  <div class="card">
-    <img src="/Photo/offers/AZEEZ RESTAURANT.jpg" alt="Product Image" class="card-image">
-    <div class="card-content">
-      <h2 class="card-title">Product Name</h2>
-      <div class="card-price">Rs. 1999/=</div>
-      <p class="card-description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
-      </p>
-      <button class="view-button">View Details</button>
-    </div>
-  </div>
 
   </div>
 
 
-   
-    
-</div>
 
-</div>
+  <div class="menu-container">
+    <p class="how-many">Showing 12 Meals</p>
+    <div class="menu-items">
 
-    
+
+      <div class="card">
+        <div class="card-lable">
+          <p>AVAILABLE</p>
+        </div>
+        <img src="/Photo/offers/AZEEZ RESTAURANT.jpg" alt="Product Image" class="card-image">
+        <div class="card-content">
+          <h2 class="card-title">Product Name</h2>
+          <div class="card-price">Rs. 1999/=</div>
+          <p class="card-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+          </p>
+          <button class="view-button">View Details</button>
+        </div>
+      </div>
+
+
+      <div class="card">
+        <div class="card-lable">
+          <p>AVAILABLE</p>
+        </div>
+        <img src="/Photo/offers/AZEEZ RESTAURANT.jpg" alt="Product Image" class="card-image">
+        <div class="card-content">
+          <h2 class="card-title">Product Name</h2>
+          <div class="card-price">Rs. 1999/=</div>
+          <p class="card-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+          </p>
+          <button class="view-button">View Details</button>
+        </div>
+      </div>
+
+
+      <div class="card">
+        <div class="card-lable">
+          <p>AVAILABLE</p>
+        </div>
+        <img src="/Photo/offers/AZEEZ RESTAURANT.jpg" alt="Product Image" class="card-image">
+        <div class="card-content">
+          <h2 class="card-title">Product Name</h2>
+          <div class="card-price">Rs. 1999/=</div>
+          <p class="card-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+          </p>
+          <button class="view-button">View Details</button>
+        </div>
+      </div>
+      
+      <div class="card">
+        <div class="card-lable">
+          <p>AVAILABLE</p>
+        </div>
+        <img src="/Photo/offers/AZEEZ RESTAURANT.jpg" alt="Product Image" class="card-image">
+        <div class="card-content">
+          <h2 class="card-title">Product Name</h2>
+          <div class="card-price">Rs. 1999/=</div>
+          <p class="card-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+          </p>
+          <button class="view-button">View Details</button>
+        </div>
+      </div>
+
+
+
+
+
+
+    </div>
+  </div>
+
+  <script src="/JavaScript/homeMenu.js"></script>
 </body>
+
 </html>
