925bb3ae2738fd4329e69a1e05bbcccd1fdc38d3
Nadun Madusanka <140165855+NadunMN@users.noreply.github.com>
2025-04-18 11:37:34 +0530

    New home page

--- views/home.php
+++ views/home.php
@@ -15,10 +15,11 @@
     <div class="home-top-main" id="home-top-main">
         <div class="first-background-img">
             <div class="first-background-overlay">
-              <div class="kottuplate">
+              <!-- <div class="kottuplate">
                 <img src="/Photo/kottu_main.png" alt="Kottu Plate" class="kottuplate-img">
-              </div>
-              <div class="square"></div>
+              </div> -->
+              <!-- <div class="square"></div> -->
+                <!-- <div class="red-squre"></div> -->
                 <div class="back-white-circle"></div>
                 <!-- <div class="pic-1">
                     <img src="/Photo/download (1).jpeg" alt="">
